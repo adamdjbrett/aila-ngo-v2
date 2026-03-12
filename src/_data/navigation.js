57a8c7440ca124f9ce3c26eafd082547940ed4fc
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2026-03-12 12:23:48 -0400

    Nav Base config (#24)
    * MVP: Add menu items to navigation data file
    
    * CONFIG: Turn burger menu on
    
    * CHANGE: Add services to top-level and rework the submenu toggle

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,20 +1,203 @@
 export default {
   top: [
     {
-      text: 'About',
-      url: '/about/'
+      text: "About",
+      url: "/about/",
+      submenu: [
+        {
+          text: 'Purpose And Vision',
+          url: '/about/mission-vision-goals/'
+        },
+        {
+          text: 'Our History',
+          url: '/about/our-history/',
+          submenu: [
+            {
+              text: 'In Memoriam: Tonya Gonnella Frichner',
+              url: '/about/our-history/in-memoriam-tonya-gonnella-frichner/'
+            },
+            {
+              text: 'League of Nations/United Nations Participation',
+              url: '/about/our-history/league-of-nations-united-nations-participation/'
+            }
+          ]
+        },
+        {
+          text: 'Staff and Board',
+          url: '/staff-and-board/'
+        },
+        {
+          text: 'Partnerships and Alliances',
+          url: '/about/partnerships-and-alliances/'
+        },
+        {
+          text: 'Contact',
+          url: '/contact/',
+          submenu: [
+            {
+              text: 'Request a Speaker',
+              url: 'https://forms.gle/1RCkLGfSyBnnmLBr5'
+            },
+            {
+              text: 'Join our email list',
+              url: '/email/'
+            },
+            {
+              text: 'Support Us',
+              url: '/support-us/'
+            }
+          ]
+        },
+      ]
     },
     {
-      text: 'Docs',
-      url: '/get-started/'
+      text: "News",
+      url: "/blog/"
     },
     {
-      text: 'Built with',
-      url: '/built-with/'
+      text: "Store",
+      url: "https://store.aila.ngo/"
     },
     {
-      text: 'Blog',
-      url: '/blog/'
+      text: "Services",
+      url: "/services/",
+      submenu: [
+        {
+          text: "Cultural Renewal & Mutual Aid",
+          url: "/programs/cultural-renewal/"
+        },
+        {
+          text: "Missing and Murdered Indigenous Relatives",
+          url: "/programs/mmiw/"
+        }
+      ]
+    },
+    {
+      text: "Programs",
+      url: "/programs/",
+      submenu: [
+        {
+          text: "Lakeback",
+          url: "/programs/lakeback/"
+        },
+        {
+          text: "Public Education",
+          url: "/programs/public-education/"
+        },
+        {
+          text: "United Nations Programming",
+          url: "/programs/united-nations-events/"
+        },
+      ]
+    },
+    {
+      text: "Engage",
+      url: "/programs/united-nations-events/",
+      submenu: [
+        {
+          text: "Blog",
+          url: "/blog/"
+        },
+        {
+          text: "Events",
+          url: "/category/events/"
+        },
+        {
+          text: "Resources",
+          url: "/links/",
+          submenu: [
+            {
+              text: "Canandaigua Treaty of 1794",
+              url: "/issues/canandaigua-treaty-of-1794/"
+            },
+            {
+              text: "Doctrine of Discovery",
+              url: "/issues/doctrine-of-discovery/"
+            },
+            {
+              text: "Original Instructions",
+              url: "/issues/original-instructions/"
+            },
+            {
+              text: "Rights of Mother Earth",
+              url: "/issues/rights-mother-earth/"
+            },
+            {
+              text: "Sovereignty",
+              url: "/issues/sovereignty/"
+            },
+            {
+              text: "Two Row Wampum",
+              url: "/issues/two-row-wampum/"
+            },
+            {
+              text: "Women’s Rights",
+              url: "/issues/womens-rights/"
+            },
+            {
+              text: "AILA Social",
+              url: "https://aila.social/"
+            },
+            {
+              text: "Onondaga Naiton",
+              url: "https://www.onondaganation.org/"
+            },
+            {
+              text: "Doctrine of Discovery Podcast",
+              url: "https://doctrineofdiscovery.org/"
+            },
+            {
+              text: "Mapping the Doctrine of Discovery Podcast",
+              url: "https://podcast.doctrineofdiscovery.org/"
+            },
+            {
+              text: "Original Free nations",
+              url: "https://originalfreenations.com/"
+            },
+            {
+              text: "Sacred places institute",
+              url: "https://www.sacredplacesinstitute.org/"
+
+            },
+            {
+              text: "Sulivan Clinton Campaign",
+              url: "https://www.sullivanclinton.com/"
+            },
+            {
+              text: "Tonatierra",
+              url: "https://www.tonatierra.org/"
+            },
+            {
+              text: "WISH-CNY",
+              url: "https://www.wishcny.org/"
+            }
+          ]
+        },
+        {
+          text: "Request a Speaker",
+          url: "https://forms.gle/1RCkLGfSyBnnmLBr5"
+        },
+        {
+          text: "What else can you do?",
+          url: "http://aila.ngo/what-else-can-you-do/"
+        }
+      ]
+    },
+    {
+      text: "Give",
+      url: "/give/"
+    },
+    {
+      text: "Facebook",
+      url: "https://www.facebook.com/americanindianlawalliance"
+    },
+    {
+      text: "Instagram",
+      url: "https://www.instagram.com/americanindianlawalliance"
+    },
+    {
+      text: "Search",
+      url: "/search/"
     }
   ],
   bottom: [
